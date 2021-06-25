bb47487477b373a7ad3aab438dc3a2bffddca961
yurii <yura2590@gmail.com>
2021-06-26 00:42:15 +0300

    Fix detecting img bug and add UL detecting

--- parser_1.py
+++ parser_1.py
@@ -9,39 +9,31 @@ from pprint import pprint
 def parse(url):
 
     response = requests.get(url)
-
     soup = BeautifulSoup(response.text, 'lxml')
-
     article = soup.find('article', class_='post')
-    
-    # print(article)
-
     data = {}
     if article.find('img', class_='post_photo_news_img'):
         data['preview'] = article.find('img', class_='post_photo_news_img').get('src')
-    # print(data['preview'])
+    else:
+        data['preview'] = None
     post_text = article.find('div', class_='post_text')
-
     data['content'] = []
     element = post_text.find('p')
-    data['content'].append({'p':element.text})
+    data['content'].append({'p':element.text.strip()})
     while element != None:
        element = element.find_next_sibling()
        if element == None: break
        if element.name == 'p':
-           data['content'].append({'p':element.text})
-    #    elif element.name == 'div' :
-    #     #    print(element)
-    #        img = element.find('img')
-    #        if img == None: continue
-    #        data['content'].append({'img':'https:'+img.get('src')})
-    
-    # pprint(data['content']) 
+           data['content'].append({'p':element.text.strip()})
+       elif element.name == 'div' and 'image-box' in element['class']:
+           img = element.find('img')
+           if img == None: continue
+           data['content'].append({'img':'https:'+img.get('src')})
+       elif element.name == 'ul':
+           data['content'].append({'ul':element.find_all('li')})
     return data
-
-  
 
 
 if __name__ == '__main__':
     
-    parse('https://www.pravda.com.ua/news/2021/06/24/7298338/')
+    parse('https://www.pravda.com.ua/news/2021/06/24/7298338/') # test link
--- rss_parser.py
+++ rss_parser.py
@@ -4,34 +4,26 @@ from pprint import pprint
 from datetime import datetime
 from time import mktime
 
-import parser_1
+from parser_1 import parse as parser
 
 
 RSS_URL = 'https://www.pravda.com.ua/rss/view_news/'
-
 feed = parse(RSS_URL)
-# pprint(feed)
-
 urls = [
     'pravda.com.ua',
     'eurointegration.com.ua',
     'life.pravda.com.ua',
     'epravda.com.ua'
 ]
-
 news = []
-
 for item in feed.entries:
     data = {}
     data['title'] = item.title
     data['description'] = item.summary
     data['url'] = item.link
     data['date'] = datetime.fromtimestamp(mktime(item.published_parsed))
-    
-
     if data['url'].split('/')[2][4:] == 'pravda.com.ua':
-        data['content'] = parser_1.parse(data['url'])
-        
+        data.update(parser(data['url']))
     news.append(data)
 
 pprint(news)
